26058ea620f6be5e2bb14ede222b9f87e4180ba5
longyu <yu.long3@56qq.com>
2021-01-25 15:41:35 +0800

    update pytest生成记录清除历史数据命令

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('pytest ./testcase -s -q --alluredir=./report/allure_results')
+os.system('pytest ./testcase -s -q --clean-alluredir=./report/allure_results')  # --clean清除历史数据
 os.system('allure generate ./report/allure_results -o ./report/allure_html --clean')
 os.system('allure serve ./report/allure_results')
